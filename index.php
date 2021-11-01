3f44142ed180e6cfc7001879226001477d3b2256
emanuelemuzio <emanuelemuzio@hotmail.it>
2021-11-01 17:10:33 +0100

    navbar wip

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <div class="sidebar">
+            <img class="navbar-logo" src="assets/images/wolf-icon.png">
+            <h1 class="navbar-center navbar-companyName">Navbar Title</h1>
+        </div>
+        <div class="main">
+            <h1>ciao</h1>
+        </div>
+    </body>
+</html>
